12ccf69573c66f844c0eea5cff7ae7571ccb552c
Student6 <Student6@10.8.33.156>
2016-10-06 12:27:22 -0400

    update

--- ChatbotPurple/src/groupFiles/BillyFpsType.java
+++ ChatbotPurple/src/groupFiles/BillyFpsType.java
@@ -6,46 +6,69 @@ public class BillyFpsType implements Topic{
 
 	private boolean inFpsLoop;
 	private boolean inDeviceLoop;
+	private boolean inMainLoop;
 	private String fpsResponse;
 
 	private static String[] consleRes = {"Eww you play on a consle", "It's 2016, pc is master race", "Consles were so 2013"};
 	private static String[] favGame = {"overwatch", "call of duty", "battlefield", "team fortress 2", "counter strike global offensive", "doom"};
+	private static String[] pcRes = {"YES PC MASTER RACE", "I'm glad you choose a pc over a consle", "I agree a pc is much better than a consle"};
 
 	public void talk() {
-		inDeviceLoop = true;
-		BillyMain.print("Oh, you like shooting games? ME TOO. What device do you play on?");
-		while(inDeviceLoop){
-			fpsResponse = BillyMain.getInput();
-			if(gameDevicePc()){
-				BillyMain.print("YESSS PC MASTER RACE");
+		inMainLoop = true;
+		while(inMainLoop){
+			inDeviceLoop = true;
+			BillyMain.print("Oh, you like shooting games? ME TOO. What device do you play on?");
+			while(inDeviceLoop){
+				fpsResponse = BillyMain.getInput();
+				if(gameDevicePc()){
+					BillyMain.print(randomFromArray(pcRes));
+				}
+				else if(fpsResponse.indexOf("stop")>=0){
+					inMainLoop = false;
+					BillyMain.talkForever();
+				}
+				else {
+					BillyMain.print(randomFromArray(consleRes));
+				}
+				inDeviceLoop = false;
+				BillyMain.print("What other thing about shooting games do you want to talk about?");
+				inFpsLoop = true;
 			}
-			else {
-				BillyMain.print(randomFromArray(consleRes));
+
+			while(inFpsLoop){	
+				fpsResponse = BillyMain.getInput();
+				int favPsn= BillyMain.findKeyword(fpsResponse, "favorite", 0);
+				if(favPsn >= 0){
+					String userFav = fpsResponse.substring(favPsn+17);
+					BillyMain.print("Cool, so your favorite game is "+userFav);
+					String botFav = randomFromArray(favGame);
+					if(userFav.equalsIgnoreCase(botFav))
+						BillyMain.print("YES, my favorite game is " +botFav+ " too!!!" );
+					else
+						BillyMain.print("That's a good choice but, I like "+botFav+" more");
+					BillyMain.print("What other thing about shooting games do you want to talk about?");
+				}
+				else if(fpsResponse.indexOf("stop")>=0){
+					exitLoop(inMainLoop);
+				}
+				else{
+					BillyMain.print("Im sorry, I'm not that advanced yet :(");
+				}
+
 			}
-			inDeviceLoop = false;
-			BillyMain.print("What other thing about shooting games do you want to talk about?");
-			inFpsLoop = true;
-		}
-		
-		while(inFpsLoop){	
-			fpsResponse = BillyMain.getInput();
-			int favPsn= BillyMain.findKeyword(fpsResponse, "favorite", 0);
-			if(favPsn >= 0){
-				String userFav = fpsResponse.substring(favPsn+17);
-				BillyMain.print("Cool, so your favorite game is "+userFav);
-				String botFav = randomFromArray(favGame);
-				if(userFav.equalsIgnoreCase(botFav))
-					BillyMain.print("YES, my favorite game is " +botFav+ " too!!!" );
-				else
-					BillyMain.print("That's a good choice but, I like "+botFav+" more");
-			}
+
 			if(fpsResponse.indexOf("stop")>=0){
-				inFpsLoop = false;
-				BillyMain.talkForever();
+				exitLoop(inMainLoop);
 			}
 		}
+
 	}
-	
+
+	private void exitLoop(boolean currentLoop){
+		currentLoop = false;
+		BillyMain.talkForever();
+	}
+
 	private String randomFromArray(String[] array){
 		int responseIndex = 0;
 		responseIndex = (int)(Math.random() * array.length);
